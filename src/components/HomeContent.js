37cc7f42c8de81e5961871e1f67dcdcda4ecdacd
faugan <foganbidi2@gmail.com>
2022-04-14 19:00:49 +0100

    changes

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,9 +1,13 @@
 import "../styles/home_content.scss";
 import "../styles/home_content_mobile.scss";
+import bg from "./img/banner.webp";
 const HomeContent=()=>{
     return(
-        <div className="home_content">
-            i am the home content
+        <div className="home_content" style={{
+            backgroundImage:`url(${bg})`,
+            backgroundRepeat: 'no-repeat',
+        }}>
+            <h2>Bienvenue</h2>
         </div>
     );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,18 +7,17 @@ import Who from "../components/Who";
 import Une from "../components/Une";
 import Informer from "../components/Informer";
 import Footer from "../components/Footer";
+import HomeContent from "../components/HomeContent";
 
 const Home=()=>{
     return(
         <div className="home">
             <Nav />
-            {/*<HomeTop />
-            <Parteners />
-            <Engagements />
-            <Who />
-            <Une />
-            <Informer />
-            <Footer />*/}
+            <div className="content">
+                <HomeContent />
+                <Footer />
+            </div>
+            
         </div>
     )
 }
