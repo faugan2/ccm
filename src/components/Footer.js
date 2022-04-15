53af7b6c28af60d2cb34b8bda62ba91d61c87209
faugan <foganbidi2@gmail.com>
2022-04-15 16:38:37 +0100

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,20 @@ import {Link} from "react-router-dom";
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import CallIcon from '@material-ui/icons/Call';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import {useDispatch,useSelector} from "react-redux";
+import {setPage,selectPage,setType,setSousType} from "../features/counterSlice";
+import {useHistory} from "react-router-dom";
+
 const Footer=()=>{
+    const history=useHistory();
+    const dispatch=useDispatch();
+
+    const show_detail=(type,sous_type)=>{
+        dispatch(setType(type));
+        dispatch(setSousType(sous_type))
+        history.push("/produits-et-services");
+    }
+
     return(
         <div className="footer">
             <div className="content">
@@ -12,7 +25,6 @@ const Footer=()=>{
                     <h4>A propos de nous</h4>
                     <ul>
                         <li><Link to="/about"><a>Qui sommes-nous ?</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Nos Produits/Services</a></Link></li>
                         <li><Link to="/valeurs-et-engagements"><a>Nos valeurs</a></Link></li>
                         
                     </ul>
@@ -20,26 +32,53 @@ const Footer=()=>{
                 <div>
                 <h4>Particuliers</h4>
                     <ul>
-                        <li><Link to="/produits-et-services"><a>Habitation</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Automobile</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Voyage</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>RC chef de famille</a></Link></li>
+                    <li onClick={show_detail.bind(this,1,1)}>
+                        <Link><a>Habitation</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,1,2)}>
+                        <Link><a>Automobile</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,1,3)}>
+                        <Link><a>Voyage</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,1,4)}>
+                        <Link><a>RC chef de famille</a></Link>
+                    </li>
                     </ul>
                 </div>
                 <div>
                 <h4>Professionnel</h4>
                     <ul>
-                        <li><Link to="/produits-et-services"><a>RC PRO</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Flotte-Automobile</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Multirisque-Professionnel</a></Link></li>
-                        <li><Link to="#"><a>Tout risque informatique</a></Link></li>
-
-                        <li><Link to="/produits-et-services"><a>Bris de marchine</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Tout risque chantier</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>RC decennale</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Santé</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Transport de marchandises</a></Link></li>
-                        <li><Link to="/produits-et-services"><a>Voyages</a></Link></li>
+                    <li onClick={show_detail.bind(this,2,1)}>
+                        <Link><a>RC PRO</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,2)}>
+                        <Link><a>Flotte-Automobile</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,3)}>
+                        <Link ><a>Multirisque professionnelle</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,4)}>
+                        <Link><a>Tout risque informatique</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,5)}>
+                        <Link><a>Bris de marchine</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,6)}>
+                        <Link><a>Tout risque chantier</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,7)}>
+                        <Link><a>RC decennale</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,8)}>
+                        <Link><a>Santé</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,9)}>
+                        <Link><a>Transport de marchandises</a></Link>
+                    </li>
+                    <li onClick={show_detail.bind(this,2,10)}>
+                        <Link><a>Voyages</a></Link>
+                    </li>
                     </ul>
                 </div>
                 <div>
