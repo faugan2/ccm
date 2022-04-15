26b63c7471061e5641a36dc94ff1e79e0db8a4cf
faugan <foganbidi2@gmail.com>
2022-04-15 16:14:44 +0100

    changes

---
+++ src/components/data.js
@@ -0,0 +1,142 @@
+const data=[
+    {
+        id:1,
+        title:"Qui sommes-nous ?",
+        content:`<ol>
+        <li>
+            <p>QUI SOMMES-NOUS</p>
+        </li>
+    </ol>
+    <p><br></p>
+    <p>Soci&eacute;t&eacute; de droit togolais agr&eacute;&eacute;e par le Minist&egrave;re de l&rsquo;Economie et des Finances sous le N&deg;029/MEF/SG/DA du 24/03/2022, CCM est une soci&eacute;t&eacute; de courtage en ASSURANCES ind&eacute;pendante qui s&rsquo;adresse &agrave; vous, particuliers, ind&eacute;pendants, dirigeants d&rsquo;entreprises et soci&eacute;t&eacute;s.<br>Son &eacute;quipe dynamique et exp&eacute;riment&eacute;e met &agrave; votre disposition, ses comp&eacute;tences afin de vous conseiller au mieux de vos int&eacute;r&ecirc;ts pour la protection de votre patrimoine et de vos r&eacute;sultats.<br>Parce qu&rsquo;il est possible de vous faire gagner du temps et de l&rsquo;argent tout en vous offrant une meilleure compr&eacute;hension des risques et de vos assurances, CCM vous propose de lui faire confiance.</p>
+    <p>Pourquoi&nbsp;choisir CCM&nbsp;?</p>
+    <ul>
+        <li>
+            <p>Notre &eacute;quipe &agrave; votre &eacute;coute</p>
+        </li>
+    </ul>
+    <p>Une demande sp&eacute;cifique ? Un doute ? Nous vous rappelons gratuitement pour r&eacute;pondre &agrave; toutes vos questions.</p>
+    <ul>
+        <li>
+            <p>Des assurances adapt&eacute;es &agrave; vos besoins</p>
+        </li>
+    </ul>
+    <p>Nous s&eacute;lectionnons aupr&egrave;s de nos partenaires l&rsquo;assurance qui vous convient le mieux : la certitude pour vous d&rsquo;&ecirc;tre bien couvert sans d&eacute;pense superflue.</p>
+    <ul>
+        <li>
+            <p>Votre fid&eacute;lit&eacute; r&eacute;compens&eacute;e</p>
+        </li>
+    </ul>
+    <p>Afin de b&eacute;n&eacute;ficier des meilleures garanties au meilleur prix, nous vous proposons une r&eacute;vision syst&eacute;matique de votre contrat tous les deux ans.</p>
+    <ul>
+        <li>
+            <p>Notre professionnalisme, gage de votre confiance</p>
+        </li>
+    </ul>
+    <p>Une &eacute;quipe comp&eacute;tente et des partenaires fiables vous garantissent d&rsquo;&ecirc;tre assur&eacute; en toute s&eacute;r&eacute;nit&eacute;.</p>`,
+    },
+    {
+        id:2,
+        type:1,
+        sous_type:1,
+        title:"Particulier/Habitation",
+        content:``
+    },
+    {
+        id:3,
+        type:1,
+        sous_type:2,
+        title:"Particulier/Automobile",
+        content:``
+    },
+    {
+        id:4,
+        type:1,
+        sous_type:3,
+        title:"Particulier/Voyage",
+        content:``
+    },
+    {
+        id:5,
+        type:1,
+        sous_type:4,
+        title:"Particulier/RC chef de famille",
+        content:``
+    },
+
+
+    {
+        id:6,
+        type:2,
+        sous_type:1,
+        title:"Professionnel/RC PRO",
+        content:``
+    },
+
+    {
+        id:7,
+        type:2,
+        sous_type:2,
+        title:"Professionnel/Flotte-Automobile",
+        content:``
+    },
+    {
+        id:8,
+        type:2,
+        sous_type:3,
+        title:"Professionnel/Multirisque professionnel",
+        content:``
+    },
+    {
+        id:9,
+        type:2,
+        sous_type:4,
+        title:"Professionnel/Tout risque informatique",
+        content:``
+    },
+    {
+        id:10,
+        type:2,
+        sous_type:5,
+        title:"Professionnel/Bris de machine",
+        content:``
+    },
+    {
+        id:11,
+        type:2,
+        sous_type:6,
+        title:"Professionnel/Tout risque chantier",
+        content:``
+    },
+
+    {
+        id:12,
+        type:2,
+        sous_type:7,
+        title:"Professionnel/RC Decennale",
+        content:``
+    },
+    {
+        id:13,
+        type:2,
+        sous_type:8,
+        title:"Professionnel/Santé",
+        content:``
+    },
+    {
+        id:14,
+        type:2,
+        sous_type:9,
+        title:"Professionnel/Transport de marchandises",
+        content:``
+    },
+    {
+        id:14,
+        type:2,
+        sous_type:10,
+        title:"Professionnel/Voyages",
+        content:``
+    },
+]
+
+export {data};
--- src/screens/About.js
+++ src/screens/About.js
@@ -7,20 +7,32 @@ import HomeContent from "../components/HomeContent";
 import { useDispatch } from "react-redux";
 import {setPage} from "../features/counterSlice";
 import {useState,useEffect} from "react";
+import {data} from "../components/data";
 
 const About =()=>{
     const dispatch=useDispatch();
+    const [title,set_title]=useState("");
+    const [content,set_content]=useState("");
 
     useEffect(()=>{
         dispatch(setPage(1))
     },[])
+
+    useEffect(()=>{
+        if(data==null) return;
+        if(data.length<0) return;
+        set_title(data[0]?.title);
+        set_content(data[0]?.content)
+    },[data]);
+
     return(
         <Layout>
             <div className="home">
                 <Nav />
             <div className="content">
                 <div className="about">
-                    
+                    <h1>{title}</h1>
+                    <div dangerouslySetInnerHTML={{__html:content}} />
                 </div>
                 <Footer />
             </div>
